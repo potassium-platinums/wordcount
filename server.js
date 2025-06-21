6e27c68ab9f3ea5f3e5171201a47a1de9fd47bf6
potassium-platinums <59970163+potassium-platinums@users.noreply.github.com>
2025-06-22 01:15:27 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ port: 443 });
 
 wss.on('connection', function connection(ws) {
   console.log('Client connected');
@@ -19,4 +19,4 @@ wss.on('connection', function connection(ws) {
   });
 });
 
-console.log('WebSocket server running on ws://localhost:8080');
+console.log('WebSocket server running on ws://wordcount-ynnd.onrender.com:443');
